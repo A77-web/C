1052d1ade7929e0b485709de97536b324b4d494c
A7- <1224717111@qq.com>
2021-05-11 17:48:20 +0800

    按位与运算

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int n = 1999;
+	int m = 2299;
+	int count = 0;
+	int tmp = n ^ m;
+	while (tmp) {
+		tmp &= (tmp - 1);
+		count++;
+	}
+	printf("%d\n", count);
+	return 0;
+}
