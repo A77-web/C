141faeb89afa870a1f16cd5e0bf00bee781bc239
A7- <1224717111@qq.com>
2021-05-19 20:30:57 +0800

    打印菱形

---
+++ 1956.C
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+# include <math.h>
+
+int main()
+{
+	int n;
+	scanf(" %d", &n);
+
+	int i = -n / 2;
+	int j;
+	while (i <= n / 2)
+	{
+		for (j = 1; j <= 2 * abs(i); ++j)
+			printf(" ");
+
+		for (j = 1; j <= n - 2 * abs(i); ++j)
+			printf("* ");
+
+		printf("\n");
+		++i;
+
+	}
+
+	return 0;
+}
