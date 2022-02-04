583d4611e05f914728f7cb1ea271cf076512cec3
A77-web <83652921+A77-web@users.noreply.github.com>
2022-02-04 20:19:28 +0800

    Add files via upload

---
+++ Common.h
@@ -0,0 +1,20 @@
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+//预防头文件的重复引入
+//#pragma once
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+//#include<vld.h>  //内存泄漏的检测工具
+#define ElemType int
+void Swap(ElemType* a, ElemType* b)
+{
+	ElemType tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+#endif /* _COMMON_H_ */
